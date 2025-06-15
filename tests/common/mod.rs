cc790bd5dc01fb679001c6ec0d73f99f3937f2ea
Leynos <leynos@troubledskies.net>
2025-06-15 11:53:47 +0100

    Document lines_vec macro

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,4 +1,8 @@
 //! Utility helpers shared across integration tests.
+/// Build a `Vec<String>` from a list of string slices.
+///
+/// This macro is primarily used in tests to reduce boilerplate when
+/// constructing example tables or other collections of lines.
 macro_rules! lines_vec {
     ($($line:expr),* $(,)?) => {
         vec![$($line.to_string()),*]
